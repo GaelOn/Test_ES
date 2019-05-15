a58ee2d85cd8a8650adc8128a5aeca6e9ee5263d
MoonRabbit <dupiregael@yahoo.fr>
2019-05-15 20:34:02 +0200

    fix restore aggregate test and add test on bad streamId

---
+++ Domain.Base.Test/ParamScenarioTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Domain.Base.Test
+{
+    public class ParamScenarioTest
+    {
+        public int ExpectedStreamId { get; }
+        public int ExpectedProcessId { get; }
+        public DateTime ExpectedDateCreated { get; }
+        public DateTime ExpectedDateStarted { get; }
+        public DateTime ExpectedDateStoped { get; }
+        public string ProcessName { get; }
+        public string ExpectedRunningService { get; }
+        public ParamScenarioTest(string processName, int expectedStreamId, int expectedProcessId,
+                                    string expectedRunningService, DateTime expectedDateCreated,
+                                    DateTime expectedDateStarted, DateTime expectedDateStoped)
+        {
+            ProcessName = processName;
+            ExpectedStreamId = expectedStreamId;
+            ExpectedProcessId = expectedProcessId;
+            ExpectedRunningService = expectedRunningService;
+            ExpectedDateCreated = expectedDateCreated;
+            ExpectedDateStarted = expectedDateStarted;
+            ExpectedDateStoped = expectedDateStoped;
+        }
+    }
+}
---
+++ Domain.Base.Test/Scenario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Domain.Mock.Implem;
+using Domain.Mock.Implem.EventFromDomain.OfInput;
+using Domain.Mock.Implem.EventFromDomain.EntityOfInput;
+
+namespace Domain.Base.Test
+{
+    public static class Scenario
+    {
+        public static InputAggregate Start_Process(ParamScenarioTest paramScenarioTest, Func<InputAggregate> aggregateFactory)
+        {
+            var aggregate = aggregateFactory();
+            var processElemCreation = new ProcessElement(paramScenarioTest.ProcessName,
+                                                         paramScenarioTest.ExpectedProcessId,
+                                                         paramScenarioTest.ExpectedDateCreated);
+            aggregate.RaiseEvent(new InputAggregateCreated(paramScenarioTest.ExpectedStreamId));
+            aggregate.RaiseEvent(new ProcessElementEntityCreated(paramScenarioTest.ExpectedStreamId,
+                                                                 processElemCreation));
+            aggregate.RaiseEvent(new ProcessElemStarted(paramScenarioTest.ExpectedStreamId,
+                                                        paramScenarioTest.ExpectedProcessId,
+                                                        paramScenarioTest.ExpectedRunningService,
+                                                        paramScenarioTest.ExpectedDateStarted));
+            return aggregate;
+        }
+        public static InputAggregate Should_Throw_Due_To_Bad_StreamId(ParamScenarioTest paramScenarioTest, Func<InputAggregate> aggregateFactory)
+        {
+            var aggregate = aggregateFactory();
+            var processElemCreation = new ProcessElement(paramScenarioTest.ProcessName,
+                                                         paramScenarioTest.ExpectedProcessId,
+                                                         paramScenarioTest.ExpectedDateCreated);
+            aggregate.RaiseEvent(new InputAggregateCreated(paramScenarioTest.ExpectedStreamId + 1));
+            //aggregate.RaiseEvent(new ProcessElementEntityCreated(paramScenarioTest.ExpectedStreamId,
+            //                                                     processElemCreation));
+            return aggregate;
+        }
+
+        public static ParamScenarioTest GetArg()
+        {
+            return new ParamScenarioTest("Test", 1, 10, "TestService", DateTime.Now, DateTime.Now.AddMinutes(1), DateTime.Now.AddMinutes(5));
+        }
+    }
+}
