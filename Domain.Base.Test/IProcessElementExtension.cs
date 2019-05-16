a68f76d822c1ec155a99fc2f76e80f7d619dbdea
MoonRabbit <dupiregael@yahoo.fr>
2019-05-17 00:06:03 +0200

    some test refactoring

---
+++ Domain.Base.Test/IProcessElementExtension.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Mock.Implem;
+using FluentAssertions;
+using Domain.Base.Test;
+
+namespace Domain.Base.AggregateBase.Test
+{
+    public static class IProcessElementExtension
+    {
+        public static void ShouldBeAsExpected(this IProcessElement pe, ParamScenarioTest param)
+        {
+            pe.Should().NotBeNull();
+            pe.RunningService.Should().Be(param.ExpectedRunningService);
+            pe.Start.Should().Be(param.ExpectedDateStarted);
+            pe.Stop.Should().Be(param.ExpectedDateStoped);
+            pe.State.Should().Be(param.ExpectedState);
+        }
+
+    }
+}
