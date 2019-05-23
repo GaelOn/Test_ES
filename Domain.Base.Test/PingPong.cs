2a308d9a45ca2b4d2248eed1dbb6a5508662d0e2
MoonRabbit <dupiregael@yahoo.fr>
2019-05-23 22:58:46 +0200

    add one kind of exception and fix setup issue in unit test introducing by adding multithreaded test (which is red for now)

---
+++ Domain.Base.Test/PingPong.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+
+namespace Domain.Base.EventSourcedAggregateRepository.Test
+{
+    public partial class EventSourcedAggregateRepositoryTest
+    {
+        public class PingPong
+        {
+            private ManualResetEvent _ping = new ManualResetEvent(true);
+            private ManualResetEvent _pong = new ManualResetEvent(false);
+
+            public void Ping()
+            {
+                _pong.Set();
+                _ping.Reset();
+                _ping.WaitOne();
+            }
+
+            public void Pong()
+            {
+                _ping.Set();
+                _pong.Reset();
+                _pong.WaitOne();
+            }
+
+            public void SetOnlyPong() => _pong.Set();
+        }
+    }
+}
---
+++ Domain.Base/Event/EventStore/Exception/StoredVersionDontMatchException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Base.Event.EventStore
+{
+    public class StoredVersionDontMatchException : EventStoreException
+    {
+        public StoredVersionDontMatchException() { }
+        public StoredVersionDontMatchException(string message) : base(message) { }
+        public StoredVersionDontMatchException(string message, Exception inner) : base(message, inner) { }
+        protected StoredVersionDontMatchException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
