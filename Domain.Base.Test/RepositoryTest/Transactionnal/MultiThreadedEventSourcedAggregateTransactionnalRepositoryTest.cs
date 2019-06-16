9fa97d5a827f92cb1a67672df1be14a9433196cc
MoonRabbit <dupiregael@yahoo.fr>
2019-06-16 23:53:08 +0200

    add one more condition to ensure that second thread action have been rollbacked

--- Domain.Base.Test/RepositoryTest/Transactionnal/MultiThreadedEventSourcedAggregateTransactionnalRepositoryTest.cs
+++ Domain.Base.Test/RepositoryTest/Transactionnal/MultiThreadedEventSourcedAggregateTransactionnalRepositoryTest.cs
@@ -66,34 +66,40 @@ namespace Domain.Base.Test.RepositoryTest.Transactionnal
             void Thread1Work()
             {
                 Thread.CurrentThread.Name = "Thread1Work";
-                var uow1 = new BasicUnitOfWork<InputAggregate, int>();
-                uow1.OnRollback += () => firstThreadFirstTaskHaveBeenRollbacked = true;
                 var _aggregate1 = _repo.GetById(param1.ExpectedStreamId);
-                pingPong.WaitPing();
-                _aggregate1.RaiseEvent(new ProcessElementEntityCreated(param1.ExpectedStreamId, processElemCreation1));
-                _aggregate1.RaiseEvent(new ProcessElemStarted(param1.ExpectedStreamId, param1.ExpectedProcessId,
-                                                            param1.ExpectedRunningService, param1.ExpectedDateStarted));
-                _aggregate1 = _saver.Save(_aggregate1, uow1);
+                using (var uow1 = new BasicUnitOfWork<InputAggregate, int>())
+                {
+                    uow1.OnRollback += () => firstThreadFirstTaskHaveBeenRollbacked = true;
+                    pingPong.WaitPing();
+                    _aggregate1.RaiseEvent(new ProcessElementEntityCreated(param1.ExpectedStreamId, processElemCreation1));
+                    _aggregate1.RaiseEvent(new ProcessElemStarted(param1.ExpectedStreamId, param1.ExpectedProcessId,
+                                                                  param1.ExpectedRunningService, param1.ExpectedDateStarted));
+                    _aggregate1 = _saver.Save(_aggregate1, uow1);
+                }
                 pingPong.Ping();
-                var uow2 = new BasicUnitOfWork<InputAggregate, int>();
-                uow1.OnRollback += () => firstThreadSecondTaskHaveBeenRollbacked = true;
-                _aggregate1.RaiseEvent(new ProcessElemStoped(param1.ExpectedStreamId, param1.ExpectedProcessId, param1.ExpectedDateStoped));
-                _aggregate1 = _saver.Save(_aggregate1, uow2);
+                using (var uow2 = new BasicUnitOfWork<InputAggregate, int>())
+                {
+                    uow2.OnRollback += () => firstThreadSecondTaskHaveBeenRollbacked = true;
+                    _aggregate1.RaiseEvent(new ProcessElemStoped(param1.ExpectedStreamId, param1.ExpectedProcessId, param1.ExpectedDateStoped));
+                    _aggregate1 = _saver.Save(_aggregate1, uow2);
+                }
                 pingPong.SetOnlyPong();
             }
             void Thread2Work()
             {
                 Thread.CurrentThread.Name = "Thread2Work";
-                var uow1 = new BasicUnitOfWork<InputAggregate, int>();
-                uow1.OnRollback += () => secondThreadFirstTaskHaveBeenRollbacked = true;
                 var _aggregate2 = _repo.GetById(param1.ExpectedStreamId);
-                pingPong.WaitPong();
-                _aggregate2.RaiseEvent(new ProcessElementEntityCreated(param2.ExpectedStreamId, processElemCreation2));
-                _aggregate2.RaiseEvent(new ProcessElemStarted(param2.ExpectedStreamId, param2.ExpectedProcessId,
-                                                            param2.ExpectedRunningService, param2.ExpectedDateStarted));
-                pingPong.Pong();
-                _aggregate2.RaiseEvent(new ProcessElemStoped(param2.ExpectedStreamId, param2.ExpectedProcessId, param2.ExpectedDateStoped));
-                _aggregate2 = _saver.Save(_aggregate2, uow1);
+                using (var uow1 = new BasicUnitOfWork<InputAggregate, int>())
+                {
+                    uow1.OnRollback += () => secondThreadFirstTaskHaveBeenRollbacked = true;
+                    pingPong.WaitPong();
+                    _aggregate2.RaiseEvent(new ProcessElementEntityCreated(param2.ExpectedStreamId, processElemCreation2));
+                    _aggregate2.RaiseEvent(new ProcessElemStarted(param2.ExpectedStreamId, param2.ExpectedProcessId,
+                                                                param2.ExpectedRunningService, param2.ExpectedDateStarted));
+                    pingPong.Pong();
+                    _aggregate2.RaiseEvent(new ProcessElemStoped(param2.ExpectedStreamId, param2.ExpectedProcessId, param2.ExpectedDateStoped));
+                    _aggregate2 = _saver.Save(_aggregate2, uow1);
+                }
             }
             void finalTask(Task t)
             {
@@ -115,6 +121,7 @@ namespace Domain.Base.Test.RepositoryTest.Transactionnal
             /// Assert
             var _aggregate = _repo.GetById(param1.ExpectedStreamId);
             _aggregate.GetProcessElementById(param1.ExpectedProcessId).ShouldBeAsExpected(param1);
+            _aggregate.GetProcessElementById(param2.ExpectedProcessId).Should().BeNull();
             finalAction.Should().NotThrow();
             firstThreadFirstTaskHaveBeenRollbacked.Should().BeFalse();
             firstThreadSecondTaskHaveBeenRollbacked.Should().BeFalse();
